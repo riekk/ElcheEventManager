60c9173f58bfb208f49e5b768137f300b6d77e37
Christian <christiantm93@gmail.com>
2024-06-05 15:53:08 +0200

    Minor ajust in css and views

--- ElcheEventManager/Controllers/HomeController.cs
+++ ElcheEventManager/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ElcheEventManager.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Making your life easier.";
 
             return View();
         }
